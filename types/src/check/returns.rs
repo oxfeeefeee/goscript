7ce9189db1da846453766818e7ac9d2a3038c2ac
oxfeeefeee <pb01005051@gmail.com>
2020-07-20 01:00:45 +0800

    returns wip

--- types/src/check/returns.rs
+++ types/src/check/returns.rs
@@ -13,8 +13,21 @@ use std::rc::Rc;
 impl<'a> Checker<'a> {
     /// is_terminating returns if s is a terminating statement.
     /// If s is labeled, label is the label name
-    pub fn is_terminating(&mut self, s: &Stmt, label: Option<String>) -> bool {
+    pub fn is_terminating(&self, s: &Stmt, label: Option<&String>) -> bool {
         unimplemented!()
+    }
+
+    fn is_terminating_list(&self, list: &Vec<Stmt>, label: Option<&String>) -> bool {
+        // trailing empty statements are permitted - skip them
+        let non_empty = list.iter().rev().find(|x| match x {
+            Stmt::Empty(_) => false,
+            _ => true,
+        });
+        if let Some(s) = non_empty {
+            self.is_terminating(s, label)
+        } else {
+            false
+        }
     }
 
     /// has_break reports if s is or contains a break statement
@@ -33,8 +46,9 @@ impl<'a> Checker<'a> {
             | Stmt::Defer(_)
             | Stmt::Return(_) => false,
             Stmt::Labeled(ls) => {
-                let l = &self.ast_ident(self.ast_objs.l_stmts[*ls].label).name;
-                self.has_break(&Stmt::Labeled(*ls), Some(l), implicit)
+                let ls_val = &self.ast_objs.l_stmts[*ls];
+                let l = &self.ast_ident(ls_val.label).name;
+                self.has_break(&ls_val.stmt, Some(l), implicit)
             }
             Stmt::Branch(bs) => {
                 bs.token == Token::BREAK
@@ -43,8 +57,26 @@ impl<'a> Checker<'a> {
                         .map_or(implicit, |ikey| Some(&self.ast_ident(ikey).name) == label))
             }
             Stmt::Block(bs) => self.has_break_list(&bs.list, label, implicit),
-
-            _ => unimplemented!(),
+            Stmt::If(ifs) => {
+                self.has_break_list(&ifs.body.list, label, implicit)
+                    || ifs.els.is_some()
+                        && self.has_break(ifs.els.as_ref().unwrap(), label, implicit)
+            }
+            Stmt::Case(cc) => self.has_break_list(&cc.body, label, implicit),
+            Stmt::Switch(ss) => {
+                label.is_some() && self.has_break_list(&ss.body.list, label, implicit)
+            }
+            Stmt::TypeSwitch(tss) => {
+                label.is_some() && self.has_break_list(&tss.body.list, label, implicit)
+            }
+            Stmt::Comm(cc) => self.has_break_list(&cc.body, label, implicit),
+            Stmt::Select(ss) => {
+                label.is_some() && self.has_break_list(&ss.body.list, label, implicit)
+            }
+            Stmt::For(fs) => label.is_some() && self.has_break_list(&fs.body.list, label, implicit),
+            Stmt::Range(rs) => {
+                label.is_some() && self.has_break_list(&rs.body.list, label, implicit)
+            }
         }
     }
 
